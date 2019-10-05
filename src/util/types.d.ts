c327d0708469124138552b830641c2f7efc09662
CaptainKO <17521098@gm.uit.edu.vn>
2019-10-05 21:33:16 +0700

    add send message

--- src/util/types.d.ts
+++ src/util/types.d.ts
@@ -1,6 +1,9 @@
+
 declare namespace Express {
   export interface Response {
     sendAndWrap(obj, key?: string): Response;
+    sendMessage(str: string): Response;
     jsonAndWrap(obj, key?: string): Response;
   }
 }
+
