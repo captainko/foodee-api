3026621ebb8b0cb3708af72e27eb4b48ad7cf243
CaptainKO <17521098@gm.uit.edu.vn>
2019-10-23 22:55:18 +0700

    remove user middleware

--- src/routes/recipe.router.ts
+++ src/routes/recipe.router.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 import { RecipeController } from "../controllers/recipe.controller";
 import { auth } from './auth';
-import { userMiddleware } from '../middleware/user.middleware';
 
 
 
@@ -10,15 +9,15 @@ const RecipeRouter = Router();
 RecipeRouter.all('*', auth.optional).param('recipe', RecipeController.preloadRecipe);
 RecipeRouter.get('/', auth.optional, RecipeController.getRecipes);
 RecipeRouter.delete('/', RecipeController.removeAll);
-RecipeRouter.post('/', auth.required, userMiddleware, RecipeController.createRecipe);
-RecipeRouter.get('/search', auth.optional, userMiddleware, RecipeController.searchRecipes);
+RecipeRouter.post('/', auth.required, RecipeController.createRecipe);
+RecipeRouter.get('/search', auth.optional, RecipeController.searchRecipes);
 RecipeRouter.get('/category/:category', RecipeController.getRecipesByCategory);
 // RecipeRouter.param('recipe',RecipeController.preloadRecipe);
-RecipeRouter.get('/:recipe/', auth.optional, userMiddleware, RecipeController.onlyPermitted, RecipeController.getRecipeByID);
-RecipeRouter.put('/:recipe/', auth.required, userMiddleware, RecipeController.onlySameUserOrAdmin, RecipeController.updateRecipe);
+RecipeRouter.get('/:recipe/', auth.optional, RecipeController.onlyPermitted, RecipeController.getRecipeByID);
+RecipeRouter.put('/:recipe/', auth.required, RecipeController.onlySameUserOrAdmin, RecipeController.updateRecipe);
 
-RecipeRouter.post('/:recipe/rating', auth.required, userMiddleware, RecipeController.rateRecipe);
-RecipeRouter.post('/:recipe/save', auth.required, userMiddleware, RecipeController.onlySameUserOrAdmin, RecipeController.saveRecipe);
+RecipeRouter.post('/:recipe/rating', auth.required, RecipeController.rateRecipe);
+RecipeRouter.post('/:recipe/save', auth.required, RecipeController.onlySameUserOrAdmin, RecipeController.saveRecipe);
 
 RecipeRouter.delete('')
 
