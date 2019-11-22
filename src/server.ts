4b773041a888e36f8a5d264eae86e5adbcbd81bb
CaptainKO <17521098@gm.uit.edu.vn>
2019-11-22 16:42:11 +0700

    add ssl

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,19 @@ import fs = require('fs');
 import https = require('https');
 // app
 import app from "./app";
-import { SERVER_PORT, SSL_PASSPHRASE } from "./environment";
+import { SERVER_PORT, SSL_PASSPHRASE, IS_PROD } from "./environment";
 
-process.on("uncaughtException", e => {
-    console.log(e);
-    process.exit(1);
-});
-
-process.on("unhandledRejection", e => {
-    console.log(e);
-    process.exit(1);
-});
+if (!IS_PROD) {
+    process.on("uncaughtException", e => {
+        console.log(e);
+        process.exit(1);
+    });
+    
+    process.on("unhandledRejection", e => {
+        console.log(e);
+        process.exit(1);
+    });
+}
 
 https.createServer({
     key: fs.readFileSync('./key.pem'),
