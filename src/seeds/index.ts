ef98d2f9e95f9713ef1989a99fa5b545b8770ce2
CaptainKO <17521098@gm.uit.edu.vn>
2019-11-28 08:06:26 +0700

    change mongo server

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,10 +1,10 @@
 // lib
-import mongoose = require('mongoose'); 
+import mongoose = require('mongoose');
 import async = require('async');
 import faker = require('faker');
 import _ = require('lodash');
 // app
-import {User, IUser  } from "../models/user.model";
+import { User, IUser } from "../models/user.model";
 import { DB_URI } from '../environment';
 import { Image, IImage } from '../models/image.model';
 import { Recipe } from '../models/recipe.model';
@@ -20,13 +20,12 @@ interface IUserData {
     created: IRecipeData[],
     saved: IRecipeData[],
   };
-  collec_: ICollectionData; 
+  collec_: ICollectionData;
 }
 
 interface IRecipeData {
   name?: string;
   description?: string;
-  status?: boolean; // true: public ~ false: private
   category?: string;
   createdBy?: string;
   servings?: number;
@@ -36,7 +35,7 @@ interface IRecipeData {
   ingredients?: { quantity: string, ingredient: string }[];
   methods?: string[];
 }
-interface ICollectionData {}
+interface ICollectionData { }
 
 const CATEGORIES = [
   'Bake',
@@ -59,10 +58,10 @@ const users: IUserData[] = [
       created: [{
 
       }],
-      saved: []
+      saved: [{
+      }]
     }
-  }
-  ,
+  },
   {
     username: faker.name.firstName() + faker.name.lastName(),
     admin: true,
@@ -76,15 +75,15 @@ const users: IUserData[] = [
       created: [{
         name: faker.name.title(),
         category: faker.random.arrayElement(CATEGORIES),
-        time: faker.random.number({min: 10, max: 200}),
+        time: faker.random.number({ min: 10, max: 200 }),
         servings: faker.random.arrayElement(SERVINGS),
         methods: fakeMethods(),
         // recipe images
         banners: fakeImages(),
         description: faker.lorem.sentences(2),
         ingredients: [
-          {quantity: '1/2 spoon', ingredient: 'sugar'},
-          {quantity: '500g', ingredient: 'beef'},
+          { quantity: '1/2 spoon', ingredient: 'sugar' },
+          { quantity: '500g', ingredient: 'beef' },
         ]
       }],
       saved: [],
@@ -99,77 +98,81 @@ const users: IUserData[] = [
 ];
 
 function fakeImages(n = 4): string[] {
-  return _.range(faker.random.number({min: 1, max: n}))
-          .map(() => faker.image.food());
+  return _.range(faker.random.number({ min: 1, max: n }))
+    .map(() => faker.image.food());
 }
 
 function fakeMethods(): string[] {
-  return _.range(faker.random.number({min: 1, max: 10}))
-          .map(() => faker.lorem.sentence());
+  return _.range(faker.random.number({ min: 1, max: 10 }))
+    .map(() => faker.lorem.sentence());
 }
-  
+
 new Promise((res) => {
-  mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-    async.parallel(users.map((user) => (done) => {
-      async.waterfall([
-        // create profile picture
-        (cb) =>  {
-          Image.create({
-            url: user.img,
-            type: 'profile',
-          })
-            .then((image) => cb(null, image))
-            .catch((err) => cb(err));
-        },
-        // create new user
-        function(image: IImage, cb)  {          
-          const newUser = new User({
-            username: user.username,
-            email: user.email,
-            isVerified: user.isVerified,
-            admin: user.admin,
-            image_url:  image.id,
-  
-          });
-          newUser.setPassword(user.password);
-          newUser.save()
-            .then((user) => cb(null, user))
-            .catch((err) => cb(err, null));
-        },
-  
-        // create recipes
-        (newUser: IUser, cb) => {
-          const recipes = user.recipes.created;
-          const createRecipeFuncs = recipes.map((r) => (rDone) => {
-            Promise.all(r.banners.map(b => {
-              return Image.create({
-                url: b,
-                type: 'recipe',
-              });
-            }))
-              .then(images => {
-                const recipe = new Recipe(r);
-                recipe.banners = images.map(x => x.id);
-                // @ts-ignore
-                recipe.createdBy = newUser.id;
-                newUser.createdRecipes.unshift(recipe.id);
-                newUser.save();
-                return recipe.save();
-              })
-              .then(newRecipe => rDone(null, newRecipe))
-              .catch(err => rDone(err, null));
-           
-          });
-          async.parallel(createRecipeFuncs, (err, recipes) => {
-            console.log(err);
-            console.log(recipes);
-            cb(recipes);
-          });
-        }
-      ], (err, results) => { console.log(err); done(); });
-    }), (err, result) => res(result));
-  }, );
- 
+  mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true , ssl: true})
+    .then(() => {
+      async.parallel(users.map((user) => (done) => {
+        async.waterfall([
+          // create profile picture
+          (cb) => {
+            Image.create({
+              url: user.img,
+              type: 'profile',
+            })
+              .then((image) => cb(null, image))
+              .catch((err) => cb(err));
+          },
+          // create new user
+          function(image: IImage, cb) {
+            const newUser = new User({
+              username: user.username,
+              email: user.email,
+              isVerified: user.isVerified,
+              admin: user.admin,
+              image_url: image.id,
+
+            });
+            newUser.setPassword(user.password);
+            newUser.save()
+              .then((user) => cb(null, user))
+              .catch((err) => cb(err, null));
+          },
+
+          // create recipes
+          (newUser: IUser, cb) => {
+            const recipes = user.recipes.created;
+            const createRecipeFuncs = recipes.map((r) => (rDone) => {
+              Promise.all(r.banners.map(b => {
+                return Image.create({
+                  url: b,
+                  type: 'recipe',
+                });
+              }))
+                .then(images => {
+                  const recipe = new Recipe(r);
+                  recipe.banners = images.map(x => x.id);
+                  // @ts-ignore
+                  recipe.createdBy = newUser.id;
+                  newUser.createdRecipes.unshift(recipe.id);
+                  newUser.save();
+                  return recipe.save();
+                })
+                .then(newRecipe => rDone(null, newRecipe))
+                .catch(err => rDone(err, null));
+
+            });
+            async.parallel(createRecipeFuncs, (err, recipes) => {
+              console.log(err);
+              console.log(recipes);
+              cb(recipes);
+            });
+          }
+        ], (err, results) => { console.log(err); done(); });
+      }), (err, result) => res(result));
+    })
+    .catch(err => {
+      console.error(err);
+    }); 
+
 }).then((value) => {
   console.log(value);
   return mongoose.disconnect();
