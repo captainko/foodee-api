38c00ab60230647edc64702d1f5023ac9f7bfdbe
CaptainKO <17521098@gm.uit.edu.vn>
2019-11-30 21:04:54 +0700

    change port

--- src/server.ts
+++ src/server.ts
@@ -18,20 +18,20 @@ import { SERVER_PORT, SSL_PASSPHRASE, IS_PROD, WEB_PORT } from "./environment";
 //     });
 // }
 
-https.createServer({
-    ca: fs.readFileSync('./ca_bundle.crt'),
-    key: fs.readFileSync('./private.key'),
-    cert: fs.readFileSync('./certificate.crt'),
-}, app).listen(SERVER_PORT);
+// https.createServer({
+//     ca: fs.readFileSync('./ca_bundle.crt'),
+//     key: fs.readFileSync('./private.key'),
+//     cert: fs.readFileSync('./certificate.crt'),
+// }, app).listen(SERVER_PORT);
 // https.createServer({
 //     ca: fs.readFileSync('./origin_ca_rsa_root.pem'),
 //     key: fs.readFileSync('./key.pem'),
 //     cert: fs.readFileSync('./cert.pem'),
 // }, app).listen(SERVER_PORT);
 
-// // app.listen(SERVER_PORT, () => {
-// //     console.log('Express server listening on port ' + SERVER_PORT);
-// // });
+app.listen(SERVER_PORT, () => {
+    console.log('Express server listening on port ' + SERVER_PORT);
+});
 // https.createServer({
 //     ca: fs.readFileSync('./ca_bundle.crt'),
 //     key: fs.readFileSync('./private.key'),
